527e0807f49e5c9ead6a8e07477829398aaf9868
AlexGoAhead <42266686+AlexGoAhead@users.noreply.github.com>
2018-08-19 15:07:36 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,8 @@
 #  Найти результат выражения для произвольных значений a,b,c: a + b * ( c / 2 )
 
-alpha = 5
-beta = 10
-gamma = 15
-delta = alpha+beta
-epsilon = gamma / 2
-zeta = delta * epsilon
-print(zeta)
+
+a = 5
+b = 10
+c = 15
+summary = a + b * (c/2)
+print('The result of equation is', summary)
